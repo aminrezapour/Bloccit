5aa8e847fb8f40d249c95195806e21f20ee64301
Amin Rezapour <aminrezapour@gmail.com>
2015-09-13 14:41:21 -0700

    Add two scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  # default ordering using scope
+  # default_scope { order('created_at DESC') }
+
+  # order by title ascending using scope
+  scope :ordered_by_title, -> { order(title: :asc) }
+  # order by time ascending using scope
+  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
