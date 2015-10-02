6f109949180a06d777b3a178cdafac328db603df
Amin Rezapour <aminrezapour@gmail.com>
2015-10-02 14:16:20 -0700

    Add post endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,9 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
-       resources :topics, only: [:index, :show]
+       resources :topics, only: [:index, :show] do
+         put :create_posts
+       end
        resources :topics, except: [:edit, :new]
        resources :posts, only: [:index, :show]
        resources :posts, except: [:edit, :new]
